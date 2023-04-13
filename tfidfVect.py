54e91eb14dbc727659966fe4862ff86f746cce25
dharmikpatel <dharmik529@gmail.com>
2023-04-12 20:56:17 -0400

    added comments

--- tfidfVect.py
+++ tfidfVect.py
@@ -1,34 +1,32 @@
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
+# import necessary module
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer 
 import pandas as pd
 import numpy as np
 import pickle
-import csv
 from sklearn.metrics.pairwise import cosine_similarity
 
-# csvfile = "tags.csv"
-# f = open(csvfile, 'r')
-# data = csv.reader(f)
+# Read the dataset into a pandas DataFrame
 data = pd.read_csv("tags.csv")
 
+# Initialize TfidfVectorizer to convert tags into TF-IDF features
 tfid = TfidfVectorizer()
 
+# Create a matrix of TF-IDF features for the tags
 vectorTfid = tfid.fit_transform(data['tags']).toarray()
+# Compute cosine similarity between each pair of tag vectors using the TF-IDF matrix
 similarityTfidfVect = cosine_similarity(vectorTfid)
 
+# Initialize CountVectorizer to count the occurrences of each word in the tags
 cv = CountVectorizer(stop_words='english', lowercase=True)
 
+# Create a matrix of count features for the tags
 vector = cv.fit_transform(data['tags']).toarray()
+# Compute cosine similarity between each pair of tag vectors using the count matrix
 similarityCountVect = cosine_similarity(vector)
 
-# hash = HashingVectorizer(lowercase=True, stop_words={'english'}, ngram_range=(1,1))
-
-# vectorHash = hash.fit_transform(data['tags']).toarray()
-# similarityHashingVect = cosine_similarity(vectorHash)
-
-
-# COMBINING SIMILARITIES
+# Combine the two similarity matrices using element-wise multiplication
 res = np.multiply(similarityTfidfVect, similarityCountVect)
-# res = np.multiply(res, similarityHashingVect)
 
+# Save the resulting similarity matrix to a binary file using pickle
 filename = 'similarity.pkl'
 pickle.dump(res, open(filename, 'wb'))
